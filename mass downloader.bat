cef5ea0963ccfd2dde319fba7206f64626e1c0a3
Zacharias Vlatakis <zvracy13@gmail.com>
2020-05-12 22:51:42 +0300

    Update mass downloader.bat

--- mass downloader.bat
+++ mass downloader.bat
@@ -1,21 +1,19 @@
 @ECHO OFF
-:: THIS BAT FILE DOWNLOADS ALL YOUR FAVORITE SOFTWARE WITH THE PRESS OF A BUTTON!
+:: THIS BAT FILE DOWNLOADS ALL YOUR FAVORITE SOFTWARE WITH THE PRESS OF A BUTTON!::
 TITLE AFTER-FORMAT TOOL
 
-start http://www.motospeed.cc/upfile/download/MotoSpeed_GamingMouse_V30(20180627).rar
+start "http://www.motospeed.cc/upfile/download/MotoSpeed_GamingMouse_V30(20180627).rar"
 start https://github.com/Sycnex/Windows10Debloater/archive/master.zip
-start https://d38w655bqoyvyi.cloudfront.net/manager/SteamGridDB+Manager+Setup+0.4.2.exe
-start https://www.ccleaner.com/speccy/download/standard
 start https://www.unifiedremote.com/download/windows
 start https://app.prntscr.com/build/setup-lightshot.exe
-start https://www.battle.net/download/getInstallerForGame?os=win&locale=enUS&version=LIVE&gameProgram=BATTLENET_APP
-start https://github.com/Nexus-Mods/Nexus-Mod-Manager/releases/download/0.71.2/Nexus.Mod.Manager-0.71.2.exe
-start https://get.popcorntime.sh/build/Popcorn-Time-0.3.10-Setup.exe
+start "https://www.battle.net/download/getInstallerForGame?os=win&locale=enUS&version=LIVE&gameProgram=BATTLENET_APP"
+start https://github.com/Nexus-Mods/Nexus-Mod-Manager/releases/download/0.71.4/Nexus.Mod.Manager-0.71.4.exe
+start https://get.popcorntime.app/build/Popcorn-Time-0.4.4-win32-Setup.exe
 start https://laptop-updates.brave.com/latest/winx64
 start https://download.onlyoffice.com/install/desktop/editors/windows/distrib/onlyoffice/DesktopEditors_x64.exe
 start https://www.microsoft.com/en-us/download/confirmation.aspx?id=35460
-start https://mega.nz/folder/wYd3WAhA#S1xotvjki5pW5rOCk6hmfg/file/YUsnHaRI
-start https://webinstallers.gog.com/download/GOG_Galaxy_2.0.exe?payload=hq25YviGiuGsCRmgp5Ww-aR6A_jCHfBD2RwJZDHUeqrwoyhbg5Txrv0oj9bAuf0AOvUTj310NFh3Oh-QczGiwiUOfSs6YX1TgRO1hP-AapMNjqBG1FFZnJr0o-I5_p7Tqnix4HH1E5Q-x7UlgXoABhp8M65xzLaG_nGe1UzVzGnWgGqynxMaBcCjG38QYMhyRuJM7VVhzlIzoFCS5-ZcFrgYKE3Jtilc1ZEYBrNlMWhNUZzu48zgouUdrZv2X3BQ3LnDA7EkiAfSAS1Jnqj6luqC0TupXbYFVau8tjxNPvQpVdW116YkFQRuEvkfKmkQik3fryifoQfZLa7unli48ExTZzIWeairXpoVDui0dz-GiRTSf6O0BQYO2Im3ylM2lSmsqPXRKXUehD-_u4V__VA1Ydp1r0R02HYdeRGe19PH_LBapP2QVjcxm3EsZHE1NlHkFypkvTSIovBam-3q6O-EmiNqHoG2ylwn_k_bzan5HjwBpBdZZEbWagYQsdIMzbrAiOt4FUJL9KVdMEzbc_VV9TAcqcXzCFtup1S8njDErp2b6kkw-zhXPrLaoLdmKv3kd8kGHnV8i-029MDaTBEtRy3DvnBO6L5_2A..
+start https://mega.nz/folder/GBsXFJzD#b_v5GhppetDXQmlh4qbLoA
+start https://webinstallers.gog.com/download/GOG_Galaxy_2.0.exe?payload=-U5662547O9qmT6_SZ55zWg6BGphNSKHbWS59QDlt1k3zST9mV6l7IkEvIymU1hq5MUYe_O5R1eF8B-74V8himV0gyvfPczTDRXVnTcCCQ732wvJ0Q..
 start https://builds.parsecgaming.com/package/parsec-windows.exe
 start https://ninite.com/classicstart-discord-firefox-irfanview-notepadplusplus-qbittorrent-steam-vlc-vscode-winrar/ninite.exe
 
