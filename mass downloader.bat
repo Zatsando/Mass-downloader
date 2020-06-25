7824744749b1f2c8f94dc5a472a00f6c1c9929fd
Zacharias Vlatakis <zvracy13@gmail.com>
2020-06-25 20:11:24 +0300

    Update mass downloader.bat

--- mass downloader.bat
+++ mass downloader.bat
@@ -2,25 +2,24 @@
 :: THIS BAT FILE DOWNLOADS ALL YOUR FAVORITE SOFTWARE WITH THE PRESS OF A BUTTON!::
 TITLE MASS DOWNLOADER TOOL
 
-start "http://www.motospeed.cc/upfile/download/MotoSpeed_GamingMouse_V30(20180627).rar"
-start https://github.com/Sycnex/Windows10Debloater/archive/master.zip
+start http://www.motospeed.cc/upfile/download/MotoSpeed_GamingMouse_V30(20180627).rar
+start https://github.com/farag2/Windows-10-Setup-Script/releases
 start https://www.unifiedremote.com/download/windows
 start https://app.prntscr.com/build/setup-lightshot.exe
-start "https://www.battle.net/download/getInstallerForGame?os=win&locale=enUS&version=LIVE&gameProgram=BATTLENET_APP"
+start https://www.battle.net/download/getInstallerForGame?os=win&locale=enUS&version=LIVE&gameProgram=BATTLENET_APP
 start https://github.com/Nexus-Mods/Nexus-Mod-Manager/releases/download/0.71.4/Nexus.Mod.Manager-0.71.4.exe
 start https://get.popcorntime.app/build/Popcorn-Time-0.4.4-win32-Setup.exe
 start https://laptop-updates.brave.com/latest/winx64
 start https://download.onlyoffice.com/install/desktop/editors/windows/distrib/onlyoffice/DesktopEditors_x64.exe
 start https://www.microsoft.com/en-us/download/confirmation.aspx?id=35460
 start https://mega.nz/folder/GBsXFJzD#b_v5GhppetDXQmlh4qbLoA
-start "https://webinstallers.gog.com/download/GOG_Galaxy_2.0.exe?payload=SRLJMgs-7mg64fzhwl4LTmU-nxu6IwuSXVWVkyureDvzA2uu1lqeEaMgFPZfJFMORuhFqqV9f3TxmSN95hamy8nhegDvzplUVaC1PvFoM4fx7WtKGoc2klrmNR2lCkXRFGCGceqsxBCSx_j1lMD3Pd_5G82kxt98BE1B6LtFRaJ_5_0LmSsTYG1eMhgHzo5DU2GBGnfMpebX1r93bcjjeT2EaBGZmgKnsJxHk5YbNyfDzU92oqcCBeV9Aub_17E_3yUaMqoeDPvwadiB458jjrMg7jUq5necQiZCmRCUuti5094y-EtkUeW_XJPGpuUu2AyJ2VvRFEVmPFMrPWAN7JPT0cdCQyVmGIxycsaETzeYO_oelYiBHzX-eksHvOjO9S4l19vn_DwDMD4CaQH6Z7kO6G-s3f4kCoRqH4MUzU5BoH7-2_ohiAInlD9YDKfy3L91FAgcIGk8U1UM3n0D1qIVOGw9cL9VIXxEHvVtXlXSseO0f2UjOVlbBY8PEmn34Jbr-Pp131BcoHRvy0wgZ_MQG59gShUpYxCnCQpixiHi72JEnOFBuiUK0VINWtoDC-Vx2Fp5BmPVoily1HLQ3nZLQ5zQEEout_H3kdmkxwrLcL89T-BOO4WVjyUSDeSF1XAiG4M2264RS5mAZGj3ivpInsNF_gYvs_KTUqEhfF8PPqTf8vo0Wy94c7k4wnDjF5bC11gzSvzVJk3CkqMCOt_cxlwtBrmaWPBeez0doqfkc2xPAg.."
+start https://webinstallers.gog.com/download/GOG_Galaxy_2.0.exe?payload=SRLJMgs-7mg64fzhwl4LTmU-nxu6IwuSXVWVkyureDvzA2uu1lqeEaMgFPZfJFMORuhFqqV9f3TxmSN95hamy8nhegDvzplUVaC1PvFoM4fx7WtKGoc2klrmNR2lCkXRFGCGceqsxBCSx_j1lMD3Pd_5G82kxt98BE1B6LtFRaJ_5_0LmSsTYG1eMhgHzo5DU2GBGnfMpebX1r93bcjjeT2EaBGZmgKnsJxHk5YbNyfDzU92oqcCBeV9Aub_17E_3yUaMqoeDPvwadiB458jjrMg7jUq5necQiZCmRCUuti5094y-EtkUeW_XJPGpuUu2AyJ2VvRFEVmPFMrPWAN7JPT0cdCQyVmGIxycsaETzeYO_oelYiBHzX-eksHvOjO9S4l19vn_DwDMD4CaQH6Z7kO6G-s3f4kCoRqH4MUzU5BoH7-2_ohiAInlD9YDKfy3L91FAgcIGk8U1UM3n0D1qIVOGw9cL9VIXxEHvVtXlXSseO0f2UjOVlbBY8PEmn34Jbr-Pp131BcoHRvy0wgZ_MQG59gShUpYxCnCQpixiHi72JEnOFBuiUK0VINWtoDC-Vx2Fp5BmPVoily1HLQ3nZLQ5zQEEout_H3kdmkxwrLcL89T-BOO4WVjyUSDeSF1XAiG4M2264RS5mAZGj3ivpInsNF_gYvs_KTUqEhfF8PPqTf8vo0Wy94c7k4wnDjF5bC11gzSvzVJk3CkqMCOt_cxlwtBrmaWPBeez0doqfkc2xPAg.."
 start https://builds.parsecgaming.com/package/parsec-windows.exe
-start https://ninite.com/adoptjdk8-classicstart-discord-firefox-irfanview-notepadplusplus-qbittorrent-steam-vlc-vscode-winrar/
-start "https://www.jetbrains.com/idea/download/download-thanks.html?platform=windows&code=IIC"
-start "https://github.com/Genymobile/scrcpy/releases/download/v1.14/scrcpy-win64-v1.14.zip"
-
+start https://ninite.com/adoptjavax8-classicstart-discord-firefox-irfanview-notepadplusplus-qbittorrent-steam-vlc-vscode-winrar/
+start https://www.jetbrains.com/idea/download/download-thanks.html?platform=windows&code=IIC
+start https://github.com/Genymobile/scrcpy/releases/download/v1.14/scrcpy-win64-v1.14.zip
+start https://playnite.link/update/stable/7.7/Playnite77.zip
+start "C:\Users\%USERNAME%\Downloads"
 @ECHO OFF
 ECHO Grats kid, your apps are here! Enjoy!
-
-start "C:\Users\%USERNAME%\Downloads"
 PAUSE
